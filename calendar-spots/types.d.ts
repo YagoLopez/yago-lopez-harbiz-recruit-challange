e6de99433b10154d123b7098116a39390cf5f308
Yago López <yago.lopez@gmail.com>
2023-12-23 23:11:40 +0100

    refactors

--- calendar-spots/types.d.ts
+++ calendar-spots/types.d.ts
@@ -1,13 +1,13 @@
-export interface TCalendar {
-  durationBefore: number;
-  durationAfter:  number;
-  slots:          TimeData;
-  sessions:       TimeData;
-}
-
 export interface Duration {
   start: string;
-  end:   string;
+  end: string;
 }
 
 type TimeData = { [key: string]: Duration[] }
+
+export interface TCalendar {
+  durationBefore: number;
+  durationAfter: number;
+  slots: TimeData;
+  sessions: TimeData;
+}
